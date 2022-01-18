e065f44531f71a4272792b49ea64cbd247a6b9d8
wrwoods-information-solutions <wrwoods-information-solutions@users.noreply.github.com>
2022-01-18 13:23:37 -0500

    Initial comit of Housing Calculator
    set up git

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed July 28 2021
+@author: WR Woods
+
+Version:  1.O.0
+"""
+from PyQt5 import QtCore, QtGui, QtWidgets
+# import sys
+# import mysql.connector
+# from mysql.connector import errorcode
+# import config as var
+
+class calculator:
+    """
+    class for calculator
+    """
+
+    def __init__(self):
+         self.comparison()
+        
+
